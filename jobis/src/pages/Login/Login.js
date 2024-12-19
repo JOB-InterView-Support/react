09176d6739454af72f0125bb400a5ad93f66aabb
Choi Man Kyeong <choimatthew0326@gmail.com>
2024-12-19 10:47:39 +0900

    login.js fix

--- jobis/src/pages/Login/Login.js
+++ jobis/src/pages/Login/Login.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom"; // Link 추가
 import { AuthContext } from "../../AuthProvider"; // AuthContext 가져오기
 import apiClient from "../../utils/axios"; // apiClient 가져오기
 import styles from "./Login.module.css";
+import kakao from "../../assets/images/kakaotalk.png";
+import naver from "../../assets/images/naver.png";
+import google from "../../assets/images/google.png";
 
 function Login() {
     const navigate = useNavigate();
@@ -126,6 +129,22 @@ function Login() {
                 >
                     {isLoggingIn ? "로그인 중..." : "로그인"}
                 </button>
+            </div>
+            <div className={styles.snsLogo}>
+                <img src={kakao} alt="kakao Logo" className={styles.logo} />
+                <img src={naver} alt="naver Logo" className={styles.logo} />
+                <img src={google} alt="google Logo" className={styles.logo} />
+            </div>
+            <div className={styles.snsName}>
+                <div className={styles.kakaoName}>카카오</div>
+                <div className={styles.naverName}>네이버</div>
+                <div className={styles.googleName}>구글</div>
+            </div>
+            <div className={styles.signupContainer}>
+                <div>아직 회원이 아니신가요?</div>
+                <div className={styles.signup}>
+                    <Link to="/signup">회원가입하기</Link>
+                </div>
             </div>
         </div>
     );
