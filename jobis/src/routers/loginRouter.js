15079ffcc9e4fda0a51f902654577c700753e9d3
Choi Man Kyeong <choimatthew0326@gmail.com>
2024-12-26 15:35:13 +0900

    google login, sign up

--- jobis/src/routers/loginRouter.js
+++ jobis/src/routers/loginRouter.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import KakaoLogin from '../pages/Login/KakaoLogin';
+import GoogleLogin from '../pages/Login/GoogleLogin';
 
 const loginRouter = [
     <Route path="/login" element={<Login />} />,
-    <Route path="/kakaoLogin" element={<KakaoLogin />} />
+    <Route path="/kakaoLogin" element={<KakaoLogin />} />,
+    <Route path="/googleLogin" element={<GoogleLogin />} />
 ];
 
 export default loginRouter;
