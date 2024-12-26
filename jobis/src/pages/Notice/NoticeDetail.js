f32d792ccb0b452e504b46fc575d0ca0a4e96950
ing-gang <coldwater.noodle@gmail.com>
2024-12-26 11:30:30 +0900

    NoticeDetail and header and list

---
+++ jobis/src/pages/Notice/NoticeDetail.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate,useParams } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider"; // AuthContext 가져오기
+import styles from "./NoticeDetail.module.css"; // CSS Modules
+import { DeleteButton } from "../../components/common/button/DeleteButton.js"
+
+
+function NoticeDetail() {
+    const { isLoggedIn, secureApiRequest, role } = useContext(AuthContext);
+    const {no} = useParams();
+    const navigate = useNavigate();
+    const [notice, setNotice] = useState(null);
+
+    const handleNoticeDetail = async () => {
+        try{
+            const response = await secureApiRequest(`/notice/detail/${no}`, {
+                method : "GET",
+            });
+            console.log("response : " + JSON.stringify(response.data));
+            setNotice(response.data);
+        } catch {
+            alert("전송실패");
+        }
+    };
+
+    const handleNoticeDelete = async () => {
+        try{
+            if (window.confirm('정말 삭제하시겠습니까 네네?')) {
+                await secureApiRequest(`/notice/delete/${no}`, {
+                    method : "delete",
+                });
+
+                navigate(-1);
+            }
+        } catch {
+            alert("전송실패");
+        }
+    }
+
+    useEffect(() => {
+        handleNoticeDetail();
+        console.log("notice : " + JSON.stringify(no));
+    }, []); 
+
+    if (!notice) {
+        return <div className={styles.loading}>로딩 중...</div>; // 로딩 표시
+    }
+
+    return (  
+        <div className={styles.noticecontainer}>
+            <h2 className={styles.noticetitle}>{notice.noticeTitle}</h2>
+
+                    <div className={styles.noticeinfo}>
+                        <span className="spacer">작성일 : {notice.noticeWDate}</span><br></br>
+                        <span className="spacer">조회수 : {notice.noticeVCount}</span>
+
+                </div>
+                <div className={styles.notciecontent}>{notice.noticeContent}</div>   
+                <br></br>                
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            <button onClick={handleNoticeDelete}>삭 제</button>
+        </div>
+    );
+}
+
+export default NoticeDetail;
--- jobis/src/routers/noticeRouter.js
+++ jobis/src/routers/noticeRouter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import AdminMemberManagement from '../pages/Admin/AdminMemberManagement';
 import NoticeList from '../pages/Notice/NoticeList';
+import NoticeDetail from '../pages/Notice/NoticeDetail';
+
 
 const noticeRouter = [
-    <Route path="/noticeList" element={<NoticeList />} />
+    <Route path="/notice" element={<NoticeList />} />,
+    <Route path="/notice/detail/:no" element={<NoticeDetail />} />
 ];
 
 export default noticeRouter;
