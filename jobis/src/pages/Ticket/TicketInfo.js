9750c356e00241471ad84dfcbc4c2d4010aec23b
KIMGEONYEOL <kgo9229@naver.com>
2025-01-02 12:23:27 +0900

    25.01.02 12:33 건열 결제 중복 요청 해결 / 중복 처리 해결중중

--- jobis/src/pages/Ticket/TicketInfo.js
+++ jobis/src/pages/Ticket/TicketInfo.js
@@ -1,11 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { AuthContext } from "../../AuthProvider"; // AuthContext 가져오기
+import { useNavigate } from "react-router-dom";
 import apiClient from "../../utils/axios";
 import styles from './TicketInfo.module.css';
 
 
 
-function TicketDetail (){
+function TicketInfo (){
+    const { isLoggedIn, isAuthInitialized, secureApiRequest, role } = useContext(AuthContext);
+    const [ticketList, setTicketList] = useState([]); // 이용권 리스트 상태
+    const [totalItems, setTotalItems] = useState(0); // 전체 아이템 수 상태
+    const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
+    const [isLoading, setIsLoading] = useState(false); // 로딩 상태
+    const [error, setError] = useState(null); // 에러 상태 관리
+
+    const navigate = useNavigate();
+
+    const fetchTicketList = async (page = 1) => {
+        setIsLoading(true);
+        setError(null);
+      
+        try {
+          const response = await secureApiRequest(`/ticket?page=${page}&size=10`, {
+            method: "GET",
+          });
+      
+          // 서버 응답 데이터 확인
+          console.log("API 응답 데이터:", response.data);
+      
+          const filteredList = response.data.list.filter(); // 필터링
+          setTicketList(filteredList);
+          setTotalItems(response.data.paging.totalItems);
+          setCurrentPage(page);
+        } catch (err) {
+          console.error("이용권 목록 로드 실패:", err);
+          setError("데이터를 불러오는 중 문제가 발생했습니다.");
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+       useEffect(() => {
+          if (!isAuthInitialized && !isLoggedIn) {
+            console.log("로그인되지 않은 상태입니다. 로그인 페이지로 이동합니다.");
+            navigate("/login");
+          } else {
+            fetchTicketList(currentPage); // 로그인 상태이면 이용권 목록 가져오기
+          }
+        }, [isLoggedIn, isAuthInitialized, navigate]);
 
     const tickets = [
        { name: "(6개월) 5+1회 이용권", price: "47000 원", date: "2024.11.01", cancelable: true },
@@ -43,4 +86,4 @@ function TicketDetail (){
     );
 }
 
-export default TicketDetail;
+export default TicketInfo;
