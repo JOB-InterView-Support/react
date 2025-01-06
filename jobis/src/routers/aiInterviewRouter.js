4f534acd7e18a7458fd1b026343b8e125e3d063d
jopanda2000 <jochihyun@kw.ac.kr>
2025-01-06 17:53:49 +0900

    aiinterviewRouter 수정정

--- jobis/src/routers/aiInterviewRouter.js
+++ jobis/src/routers/aiInterviewRouter.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import SelectIntro from "../pages/AiInterview/SelectIntro";
 import InterviewPage from "../pages/AiInterview/InterviewPage";
+import InterviewTest from '../pages/AiInterview/InterviewTest';
 
 const aiInterviewRouter = [
   <Route path="/selectintro" element={<SelectIntro />} />,
   <Route path="/interview/:intro_no/:round" element={<InterviewPage />} />,
+  <Route path="/interviewtest" element={<InterviewTest />} />,
 ];
 
 export default aiInterviewRouter;
+
