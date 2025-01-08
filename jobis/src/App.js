13054f6ae115fe31e384dd5e79c4b7ec3a121fa6
Choi Man Kyeong <choimatthew0326@gmail.com>
2025-01-08 10:40:55 +0900

    ResultFooter.js

--- jobis/src/App.js
+++ jobis/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import AppRouter from './routers/router';
-
+import React, { useState } from "react";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import AppRouter from "./routers/router";
+import ResultFooter from "./components/common/ResultFooter";
 
 function App() {
+  const [resultData, setResultData] = useState(null);
+
   return (
     <>
-      <Header />      
-      <AppRouter /> 
+      <Header />
+      <AppRouter setResultData={setResultData} />
+      {resultData && (
+        <ResultFooter
+          filename={resultData.filename}
+          introNo={resultData.intro_no}
+          roundId={resultData.round_id}
+          intId={resultData.int_id}
+        />
+      )}
       <Footer />
     </>
   );
 }
 
 export default App;
-
