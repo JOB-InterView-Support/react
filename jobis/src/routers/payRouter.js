19065ecd7c27791ea17023132cd9406a2416a583
KIMGEONYEOL <kgo9229@naver.com>
2025-01-15 18:21:48 +0900

    25.01.15 1821 건열 불필요한 page 삭제 및 router 수정 (서버 구동 오류 없었음)

--- jobis/src/routers/payRouter.js
+++ jobis/src/routers/payRouter.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import PaymentList from "../pages/Payment/PaymentList";
-import PaymentDetail from "../pages/Payment/PaymentDetail";
 import PaymentSuccess from "../pages/Payment/PaymentSuccess";
 
 const PaymentRouter = [
-    <Route path="/paymentList" element={<PaymentList/>}/>,
-    <Route path="/paymentDetail" element={<PaymentDetail/>}/>,
     <Route path="/paymentSuccess" element={<PaymentSuccess/>}/>,
 ];
 
