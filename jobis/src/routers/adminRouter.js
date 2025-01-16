fede4bffda78d09b9c17c5e00c0e7341a2fdddcf
Choi Man Kyeong <choimatthew0326@gmail.com>
2025-01-16 10:53:08 +0900

    adinrouter fix

--- jobis/src/routers/adminRouter.js
+++ jobis/src/routers/adminRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import AdminMemberManagement from '../pages/Admin/AdminMemberManagement';
 import AdminMemberManagementDetail from '../pages/Admin/AdminMemberManagementDetail';
-import AdminStatistics from '../pages/Admin/AdminStatistics';
+
 import AdminSalesHistory from '../pages/Admin/AdminSalesHistory';
 import AdminCommonQuestions from '../pages/Admin/AdminCommonQuestions';
 import AdminInsertCommonQuestions from '../pages/Admin/AdminInsertCommonQuestions';
@@ -11,7 +11,7 @@ import AdminCommonQuestionsDetail from '../pages/Admin/AdminCommonQuestionsDetai
 const adminRouter = [
     <Route path="/adminMemberManagement" element={<AdminMemberManagement />} />,
     <Route path="/adminMemberDetail" element={<AdminMemberManagementDetail />} />,
-    <Route path="/adminStatistics" element={<AdminStatistics />} />,
+
     <Route path="/adminSalesHistory" element={<AdminSalesHistory />} />,
     <Route path="/adminCommonQuestions" element={<AdminCommonQuestions />} />,
     <Route path="/adminInsertCommonQuestions" element={<AdminInsertCommonQuestions />} />,
